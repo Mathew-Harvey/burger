a6d6cbdfd533f8846f395e783d9333c619e5dcf0
Mathew <mathewharvey@gmail.com>
2020-11-15 20:33:15 +0800

    added code structure and default template code

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name) VALUES ('Royal With Cheese');
+INSERT INTO burgers (burger_name) VALUES ('Pickle and Mustard');
+INSERT INTO burgers (burger_name) VALUES ('The Heartattack');
